8c58f5ecaba65593ed1f6e90f4068d70449a7b89
Casper <casperbramm@hotmail.com>
2023-11-17 14:59:44 +0100

    nu virker oprettelse af header file of generelt brug af en setup sketch

--- arduino_code/Auto-Detect/board_detection/init_sketch/arduino_secrets.h
+++ arduino_code/Auto-Detect/board_detection/init_sketch/arduino_secrets.h
@@ -1,6 +1,6 @@
 
-    #define SECRET_SSID "hej"
-    #define SECRET_PASS "ok"
+    #define SECRET_SSID "marc"
+    #define SECRET_PASS "anna"
     #define SECRET_STOPIC "IDIoT/device_1"
     #define SECRET_MQTTUN "mqtt_username"
     #define SECRET_MQTTPW "mqtt_password"
