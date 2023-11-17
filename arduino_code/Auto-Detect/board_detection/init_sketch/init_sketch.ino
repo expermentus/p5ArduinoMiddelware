75ea2c2db68ff0057af44c3d075b864487f1f1aa
Casper <casperbramm@hotmail.com>
2023-11-17 14:51:06 +0100

    nu virker oprettelse af header file of generelt brug af en setup sketch

--- arduino_code/Auto-Detect/board_detection/init_sketch/init_sketch.ino
+++ arduino_code/Auto-Detect/board_detection/init_sketch/init_sketch.ino
@@ -1,10 +1,20 @@
-
 #include <ArduinoMqttClient.h>
 #include <WiFiNINA.h>
 #include "arduino_secrets.h"
 #include <Arduino_MKRIoTCarrier.h>
 MKRIoTCarrier carrier;
 
+void setup() {
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = toilet;        // your network SSID (name)
-char pass[] = børste;    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+char stopic[] = SECRET_STOPIC;
+char mqttun[] = SECRET_MQTTUN;
+char mqttpw[] = SECRET_MQTTPW;
+  carrier.begin();
+  carrier.display.fillScreen(0xF800); // red
+  }
+
+void loop() {
+
+}
