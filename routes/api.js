181a15b305dceb285a5953e775f77acb8521a4e2
Anna Christiansen <annagc21@student.aau.dk>
2023-11-21 12:29:25 +0100

    publish .bin til azure

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ const mysql = require('mysql2');
 const fs = require('fs');
 const env = require('dotenv');
 const {getConnection} = require("../connectionManager");
+const {BlobServiceClient} = require("@azure/storage-blob");
 
 
 router.post('/test', function(req, res, next) {
@@ -40,5 +41,42 @@ router.get('/flag', function(req, res, next) {
     res.json({ message: 'HKN:{68t73458769q32gyuhaf}' });
 });
 
+router.get('/upload', function (req, res, next)  {
+    const { BlobServiceClient } = require("@azure/storage-blob");
+
+// Set your Azure Storage connection string
+    const connectionString = "DefaultEndpointsProtocol=https;AccountName=p5test;AccountKey=DvxHnZVcWYvIxm7iqlbhyI8ngBwbTxaJGXR4pArlA47vJIqgGAaSW6HNgrCuv4vTln0KhFzIgnaN+AStsKrPSg==;EndpointSuffix=core.windows.net\n";
+
+// Create BlobServiceClient
+    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+
+// Set the name of the container where you want to upload the file
+    const containerName = "binfiles";
+
+// Set the name of the .bin file
+    const blobName = "your-file.bin";
+
+// Get a reference to the container
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+// Get a block blob client
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+// Specify the path to your local .bin file
+    const filePath = "/hej.bin";
+
+// Upload the .bin file to Azure Storage
+    async function uploadToAzureStorage() {
+        try {
+            const response = await blockBlobClient.uploadFile(filePath);
+            console.log("File uploaded successfully:", response.requestId);
+        } catch (error) {
+            console.error("Error uploading file to Azure Storage:", error.message);
+        }
+    }
+
+// Call the upload function
+    uploadToAzureStorage();
+});
 
 module.exports = router;
