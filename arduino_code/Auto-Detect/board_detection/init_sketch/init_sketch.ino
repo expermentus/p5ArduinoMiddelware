b78297371d4372c2eb9ec5539f8b2d261350a063
Anna Christiansen <annagc21@student.aau.dk>
2023-11-23 16:33:32 +0100

    arduino ota secure

--- arduino_code/Auto-Detect/board_detection/init_sketch/init_sketch.ino
+++ arduino_code/Auto-Detect/board_detection/init_sketch/init_sketch.ino
@@ -115,16 +115,6 @@ void handleSketchDownload(char PATH[]) {
   Serial.flush();
   InternalStorage.apply(); // this doesn't return
   Serial.println("done");
-
-    // Call the function and pass the message buffer
-    handleSketchDownload(messageBuffer);
-  }
-  // use the Stream interface to print the contents
-  while (mqttClient.available()) {
-    Serial.print((char)mqttClient.read());
-  }
-  Serial.println();
-  Serial.println();
 }
 
 void setup() {
