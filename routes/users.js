a21e72163ffd0584f4ca34cc25760f0f243ce46d
TheChef42 <danielflute@gmail.com>
2023-11-29 21:59:28 +0100

    mqqt push a fil

--- routes/users.js
+++ routes/users.js
@@ -4,31 +4,31 @@ const mqtt = require('mqtt');
 const connectionManager = require('../connectionManager');
 
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   const connection = connectionManager.getConnection();
-
   // Query to select all devices from the 'devices' table
   const devicesQuery = 'SELECT * FROM devices';
-
   // Query to select data from the 'sensor_data' table joined with 'devices'
   const sensorDataQuery = 'SELECT devices.name AS device_name, sensor_data.data_name, sensor_data.reading FROM devices RIGHT JOIN sensor_data ON devices.id = sensor_data.device_id';
-
-  // Execute both queries using Promise.all
-  Promise.all([
-    queryPromise(connection, devicesQuery),
-    queryPromise(connection, sensorDataQuery)
-  ])
-      .then(([devicesResults, sensorDataResults]) => {
-        res.render('heartBeat', { devices: devicesResults, sensorData: sensorDataResults });
-      })
-      .catch(error => {
-        console.error('Error loading data:', error);
-        res.status(500).send('Error loading data');
-      });
+  try {
+    // Execute both queries using Promise.all
+    const [devicesResults, sensorDataResults] = await Promise.all([
+      queryPromise(connection, devicesQuery),
+      queryPromise(connection, sensorDataQuery)
+    ]);
+    // Render the view with the obtained results
+    res.render('heartBeat', { devices: devicesResults, sensorData: sensorDataResults });
+  } catch (error) {
+    console.error('Error loading data:', error);
+    res.status(500).send('Error loading data');
+  } finally {
+    // Release the connection when done
+    connection.close();
+  }
 });
 
 // Function to execute a query and return a promise
-function queryPromise(connection, query) {
+async function queryPromise(connection, query) {
   return new Promise((resolve, reject) => {
     connection.query(query, (error, results) => {
       if (error) {
