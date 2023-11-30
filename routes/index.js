52df905aec77144b4c00b9151b220944b3de451e
expermentus <christofferbakaa@gmail.com>
2023-11-30 13:51:01 +0100

    device setup (outdate status)

--- routes/index.js
+++ routes/index.js
@@ -26,53 +26,53 @@ router.get('/deviceSetup', function(req, res, next) {
   console.log(arduinos)
 });
 
-router.post('/deviceSetup', async(req, res) => {
-  const connection = connectionManager.getConnection();
-  const query = 'SELECT name FROM devices';
-  const { name, ssid, ssid_pass, serial} = req.body;
-  const status = "Configured"
-  var countSameName = 0;
-  var topic;
+router.post('/deviceSetup', async (req, res) => {
+  try {
+    const connection = connectionManager.getConnection();
+    const { name, ssid, ssid_pass, serial } = req.body;
+    const status = "Configured";
+    let countSameName = 0;
+    let topic;
 
-  connection.query(query, (error, results)=> {
-    if (error) {
-      console.error('Error loading devices:', error);
-      res.status(500).send('Error loading devices');
-      return;
-    }
-
+    // Update existing device status to 'outdated' based on serial
     const updatesql = `UPDATE devices SET status = 'outdated' WHERE serial = '${serial}'`;
+    const outdate = new Promise((resolve, reject) => {
+      connection.query(updatesql, (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
 
-    getConnection().query(updatesql, (err, result) => {
-      if (err) {
-        console.error('Error inserting data into the database:', err);
-        res.status(500).json({message: 'Error inserting data into the database'});
-        return;
-      }
-      console.log('Data inserted into the database');
-    })
-  })
+    // Wait for the outdate promise to complete before proceeding
+    await outdate;
 
-  connection.query(query, (error, results) => {
-    if (error) {
-      console.error('Error loading devices:', error);
-      res.status(500).send('Error loading devices');
-      return;
-    }
+    // Check for devices with the same name
+    const selectSql = `SELECT * FROM devices WHERE name = '${name}'`;
+    const selectResults = await new Promise((resolve, reject) => {
+      connection.query(selectSql, (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
 
-    for (let i = 0; i < results.length; i++) {
-      if (name === results[i].name) {
-        countSameName += 1;
-      }
-    }
+    // Count devices with the same name
+    countSameName = selectResults.length;
+
+    // Generate a unique topic based on the name and count
     topic = name + countSameName;
     topic = topic.replace(/[^a-zA-Z0-9]/g, '');
 
     // Insert data into the database
-    const sql = 'INSERT INTO devices (name, ssid, topic, serial, status) VALUES (?, ?, ?, ?, ?)';
-    const values = [name, ssid, topic, serial, status];
+    const insertSql = 'INSERT INTO devices (name, ssid, topic, serial, status) VALUES (?, ?, ?, ?, ?)';
+    const insertValues = [name, ssid, topic, serial, status];
 
-    getConnection().query(sql, values, (err, result) => {
+    connection.query(insertSql, insertValues, (err, result) => {
       if (err) {
         console.error('Error inserting data into the database:', err);
         res.status(500).json({ message: 'Error inserting data into the database' });
@@ -80,16 +80,32 @@ router.post('/deviceSetup', async(req, res) => {
       }
 
       console.log('Data inserted into the database');
-      insertedData = {
+      const insertedData = {
         choice: '1',
         ssid: ssid,
         password: ssid_pass,
         stopic: topic,
         serial: serial,
       };
-      res.render('deviceSetup', { title: 'Device Setup Page', alert: 'data stored succesfully' });
+      res.render('deviceSetup', { title: 'Device Setup Page', alert: 'Data stored successfully' });
+    });
+  } catch (error) {
+    console.error('An error occurred:', error);
+    res.status(500).json({ message: 'An error occurred' });
+  }
+});
+
+
+function queryPromise(connection, query) {
+  return new Promise((resolve, reject) => {
+    connection.query(query, (error, results) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(results);
+      }
     });
   });
-});
+}
 
 module.exports = router;
